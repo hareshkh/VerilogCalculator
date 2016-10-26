dc352e214c3d3c8a3cc0faad2042c61762d91c18
Haresh Khanna <khanna.haresh@yahoo.co.in>
2016-10-26 19:17:51 +0530

    Adds 1 bit adder

---
+++ adder1bit.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+/*
+ * Group_ID-61 (15114031_15114032) - Haresh Khanna & Harjot Singh Oberai
+ * Date: October 25, 2016
+ * adder1bit.v
+ *
+ */
+
+module adder1bit(
+	input a,
+	input b,
+	input cin,
+	output sum,
+	output cout
+	);
+
+	assign sum = a ^ b ^ cin;
+	assign cout = (a & b)  |  (a & cin)  |  (b & cin);
+
+endmodule
